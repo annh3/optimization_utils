ad5d74273abedb8f8c417e98b5ba3a48d85218da
Ann He <annhe@Anns-MacBook-Pro.local>
2021-06-19 11:05:01 -0700

    fixed and validated line search

--- bls.py
+++ bls.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-def line_search(f, x_c, search_dir, gamma, grad_dir, tol):
-    t = 1
-    f_c = f(x_c)
-    new_f = f(x_c + search_dir)
-    derphi = np.dot(grad_dir, search_dir)
-    while new_f > f_c + t*derphi + tol:
-        t = gamma * t
-        new_f = f(x_c + t*search_dir)
-    return t
+def line_search(f, x_c, c, search_dir, rho, grad_dir, tol=1e-3, max_iter=100):
+    alpha = 1
+    iter = 0
+    while iter < max_iter and f(x_c + alpha * search_dir) > f(x_c) + (c * alpha * grad_dir.T @ search_dir) + tol:
+        alpha = rho * alpha
+        iter = iter + 1
+        if iter % 20 == 0:
+        	print("iter: ", iter)
+        	print("lhs: ", f(x_c + alpha * search_dir))
+        	print("rhs: ", f(x_c) + (c * alpha * grad_dir.T @ search_dir) + tol)
+        	print("\n")
+    return alpha, f(x_c + alpha * search_dir)
